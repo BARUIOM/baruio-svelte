634dc63ee8d4e176b603f7309a5922e1ad5cb556
listanegra <5874907+listanegra@users.noreply.github.com>
2021-11-14 23:44:46 -0300

    Basic main App component implementation

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<main>
+    <div class="flex justify-center items-center w-screen h-screen">
+        <div class="text-4xl text-red-700 font-bold">Hello World</div>
+    </div>
+</main>
+
+<style global lang="postcss">
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
+</style>
